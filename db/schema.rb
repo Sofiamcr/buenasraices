443e4ce429ec925fde3a3358a781ca8537eaa8aa
Sofia CachoRuschner <sofiamcr@gmail.com>
2023-02-02 19:12:07 +0100

    adding structure and basic css to category_fow page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_113904) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_133147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_113904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.text "description"
   end
 
   create_table "proyects", force: :cascade do |t|
