66f2d52b5e8fc564d990db78002e61e7ecbc7119
Sofia CachoRuschner <sofiamcr@gmail.com>
2023-02-02 11:47:09 +0100

    add new table and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.create(name: "Muelles", image_url: "")
+Category.create(name: "Deck", image_url: "")
+Category.create(name: "Fogones", image_url: "")
+Category.create(name: "Huertas", image_url: "")
+Category.create(name: "Pergolas", image_url: "")
+Category.create(name: "Tablestacados", image_url: "")
+Category.create(name: "Cerco y Empalizada", image_url: "")
