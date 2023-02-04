84c345269aba813de6cf9ee90c93f5d59addea6a
Sofia CachoRuschner <sofiamcr@gmail.com>
2023-02-04 23:53:13 +0100

    adding some front in the categories show page

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,17 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+
+
+// let carousel = document.querySelector('.carousel');
+// let left = document.querySelector('.left');
+// let right = document.querySelector('.right');
+// let item = document.querySelector('.item');
+// (function () {
+//    right.addEventListener('click', function (e) {
+//       carousel.scrollLeft += item.clientWidth;
+//    });
+//    left.addEventListener('click', function () {
+//       carousel.scrollLeft -= item.clientWidth;
+//    });
+//https://dev.to/kevinkh89/carousel-with-little-to-zero-javascript-using-flexbox-and-overflow-37il
